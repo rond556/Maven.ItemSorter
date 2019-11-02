90d8a931e76161f609055ee7df65bb22154536e1
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-02 15:04:57 -0400

    finished

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,17 +1,23 @@
 package rocks.zipcode.io;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    private Item[] items;
 
     public ItemSorter(Item[] items) {
         this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Arrays.sort(items, comparator);
+
+        return items;
     }
 }
